0a5c0e9507eaa17f3db61db16e08efaf69cc09d7
kklibo <kklibo4@gmail.com>
2021-06-21 19:17:37 -0700

    dispute + resolve + chargeback implementation

--- src/engine.rs
+++ src/engine.rs
@@ -1,6 +1,6 @@
 //! The transaction processing engine
 
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap,HashMap,HashSet};
 use std::error::Error;
 
 use crate::input::InputRecord;
@@ -12,7 +12,7 @@ use crate::account_state::AccountState;
 pub struct ClientId(pub u16);
 
 /// A globally-unique transaction ID
-#[derive(Copy,Clone,PartialEq,Debug)]
+#[derive(Copy,Clone,Eq,PartialEq,Hash,Debug)]
 struct TxId(u32);
 
 /// A deposit or withdrawal amount; expected precision is 4 places past the decimal
@@ -24,6 +24,9 @@ pub struct Amount(pub f32);
 enum Transaction {
     Deposit(TxId, Amount),
     Withdrawal(TxId, Amount),
+    Dispute(TxId),
+    Resolve(TxId),
+    Chargeback(TxId),
 }
 
 /// Parses an InputRecord into a client ID + Transaction pair
@@ -39,9 +42,18 @@ fn parse_record(record: &InputRecord) -> Result<(ClientId, Transaction), Box<dyn
                 _ => Err("invalid input record".into())
 
             }
-        }
+        },
 
-        _ => Err("invalid input record".into())
+        InputRecord{r#type, client,tx, amount: None} => {
+
+            match r#type.as_str() {
+                "dispute"    => Ok((ClientId(*client), Transaction::Dispute(TxId(*tx)))),
+                "resolve"    => Ok((ClientId(*client), Transaction::Resolve(TxId(*tx)))),
+                "chargeback" => Ok((ClientId(*client), Transaction::Chargeback(TxId(*tx)))),
+                _ => Err("invalid input record".into())
+            }
+        },
+
     }
 }
 
@@ -57,23 +69,86 @@ fn process_account_transactions(client_id: ClientId, transactions: &Vec<Transact
         locked: false
     };
 
+    //for existing deposits: transaction IDs mapped to amounts
+    let mut deposit_amounts = HashMap::<TxId, Amount>::new();
+
+    //the transaction IDs of disputed deposits
+    let mut disputed_deposit_ids = HashSet::<TxId>::new();
+
     for transaction in transactions {
 
         match transaction {
 
-            Transaction::Deposit(_tx_id, amount) => {
+            &Transaction::Deposit(tx_id, amount) => {
 
                 //deposits always succeed
                 account_state.available.0 += amount.0;
+
+                //record this deposit, in case of a chargeback
+                // note: this assumes transaction ID uniqueness: no check for insert() overwrite
+                deposit_amounts.insert(tx_id, amount);
             },
 
-            Transaction::Withdrawal(_tx_id, amount) => {
+            &Transaction::Withdrawal(_tx_id, amount) => {
 
                 //withdrawals only happen if enough funds are available
                 if account_state.available.0 >= amount.0 {
                     account_state.available.0 -= amount.0;
                 }
             },
+
+            &Transaction::Dispute(tx_id) => {
+
+                //disputes only happen on existing deposits
+                if let Some(&amount) = deposit_amounts.get(&tx_id) {
+
+                    //hold the disputed funds
+                    account_state.available.0 -= amount.0;
+                    account_state.held.0 += amount.0;
+
+                    //record the disputed status
+                    // note: this assumes transaction ID uniqueness: no check for insert() overwrite
+                    disputed_deposit_ids.insert(tx_id);
+                }
+            },
+
+            &Transaction::Resolve(tx_id) => {
+
+                //resolve only applies to an existing disputed deposit
+                if let Some(&amount) = deposit_amounts.get(&tx_id) {
+
+                    if disputed_deposit_ids.contains(&tx_id) {
+
+                        //make the disputed funds available
+                        account_state.available.0 += amount.0;
+                        account_state.held.0 -= amount.0;
+                    }
+
+                    //remove the disputed status
+                    disputed_deposit_ids.remove(&tx_id);
+                }
+            },
+
+            &Transaction::Chargeback(tx_id) => {
+
+                //chargeback only applies to an existing disputed deposit
+                if let Some(&amount) = deposit_amounts.get(&tx_id) {
+
+                    if disputed_deposit_ids.contains(&tx_id) {
+
+                        //remove the chargeback withdrawal from held funds
+                        account_state.held.0 -= amount.0;
+
+                        //lock (also "freeze") this account
+                        account_state.locked = true;
+
+                        //now that the client account is locked, no more actions are possible:
+                        // return the current account state, ignoring all remaining transactions
+                        return account_state;
+                    }
+                }
+            },
+
         }
     }
 
@@ -133,6 +208,30 @@ mod test {
             assert_eq!(result, (ClientId(1), Transaction::Withdrawal(TxId(2), Amount(3.0))));
         }
 
+        //success: dispute
+        {
+            let record = InputRecord{r#type: "dispute".to_string(), client: 1, tx: 2, amount: None};
+            let result = parse_record(&record).unwrap();
+
+            assert_eq!(result, (ClientId(1), Transaction::Dispute(TxId(2))));
+        }
+
+        //success: resolve
+        {
+            let record = InputRecord{r#type: "resolve".to_string(), client: 1, tx: 2, amount: None};
+            let result = parse_record(&record).unwrap();
+
+            assert_eq!(result, (ClientId(1), Transaction::Resolve(TxId(2))));
+        }
+
+        //success: chargeback
+        {
+            let record = InputRecord{r#type: "chargeback".to_string(), client: 1, tx: 2, amount: None};
+            let result = parse_record(&record).unwrap();
+
+            assert_eq!(result, (ClientId(1), Transaction::Chargeback(TxId(2))));
+        }
+
         //failure: nonexistent transaction type
         {
             let record = InputRecord{r#type: "no_such_tx_type".to_string(), client: 1, tx: 2, amount: None};
@@ -144,6 +243,14 @@ mod test {
         //failure: deposit is missing its amount
         {
             let record = InputRecord{r#type: "deposit".to_string(), client: 1, tx: 2, amount: None};
+            let result = parse_record(&record);
+
+            assert!(matches!(result, Err(_)));
+        }
+
+        //failure: dispute has an amount
+        {
+            let record = InputRecord{r#type: "dispute".to_string(), client: 1, tx: 2, amount: Some(3.0)};
             let result = parse_record(&record);
 
             assert!(matches!(result, Err(_)));
@@ -211,6 +318,119 @@ mod test {
 
             assert_eq!(result, expected);
         }
+
+        //pending dispute (neither resolved nor charged back)
+        {
+            let transactions = vec![
+                Transaction::Deposit(TxId(1), Amount(10.0)),
+                Transaction::Dispute(TxId(1)),
+            ];
+
+            let expected = AccountState {
+                client_id,
+                available: Amount(0.0),
+                held: Amount(10.0),
+                locked: false
+            };
+
+            let result = process_account_transactions(client_id, &transactions);
+
+            assert_eq!(result, expected);
+        }
+
+        //resolved dispute
+        {
+            let transactions = vec![
+                Transaction::Deposit(TxId(1), Amount(10.0)),
+                Transaction::Dispute(TxId(1)),
+                Transaction::Resolve(TxId(1)),
+            ];
+
+            let expected = AccountState {
+                client_id,
+                available: Amount(10.0),
+                held: Amount(0.0),
+                locked: false
+            };
+
+            let result = process_account_transactions(client_id, &transactions);
+
+            assert_eq!(result, expected);
+        }
+
+        //chargeback with blocked subsequent transaction attempts
+        {
+            let transactions = vec![
+                Transaction::Deposit(TxId(1), Amount(10.0)),
+                Transaction::Dispute(TxId(1)),
+                Transaction::Chargeback(TxId(1)),
+
+                //remaining transactions will not happen (account is locked/frozen)
+                Transaction::Resolve(TxId(1)),
+                Transaction::Deposit(TxId(1), Amount(100.0)),
+                Transaction::Withdrawal(TxId(1), Amount(5.0)),
+            ];
+
+            let expected = AccountState {
+                client_id,
+                available: Amount(0.0),
+                held: Amount(0.0),
+                locked: true
+            };
+
+            let result = process_account_transactions(client_id, &transactions);
+
+            assert_eq!(result, expected);
+        }
+
+        //dispute resolution precedes deposit
+        {
+            let transactions = vec![
+                //these transactions have no effect, their target doesn't exist yet
+                Transaction::Dispute(TxId(1)),
+                Transaction::Chargeback(TxId(1)),
+                Transaction::Resolve(TxId(1)),
+                //
+
+                Transaction::Deposit(TxId(1), Amount(10.0)),
+            ];
+
+            let expected = AccountState {
+                client_id,
+                available: Amount(10.0),
+                held: Amount(0.0),
+                locked: false
+            };
+
+            let result = process_account_transactions(client_id, &transactions);
+
+            assert_eq!(result, expected);
+        }
+
+        //dispute resolution precedes dispute
+        {
+            let transactions = vec![
+                Transaction::Deposit(TxId(1), Amount(10.0)),
+
+                //these transactions have no effect, their target isn't disputed yet
+                Transaction::Chargeback(TxId(1)),
+                Transaction::Resolve(TxId(1)),
+                //
+
+                Transaction::Dispute(TxId(1)),
+            ];
+
+            let expected = AccountState {
+                client_id,
+                available: Amount(0.0),
+                held: Amount(10.0),
+                locked: false
+            };
+
+            let result = process_account_transactions(client_id, &transactions);
+
+            assert_eq!(result, expected);
+        }
     }
 
 
@@ -257,6 +477,71 @@ mod test {
                 AccountState{client_id: ClientId(1), available: Amount(10.0), held: Amount(0.0), locked: false},
                 AccountState{client_id: ClientId(2), available: Amount(2.0), held: Amount(0.0), locked: false},
                 AccountState{client_id: ClientId(3), available: Amount(8.0), held: Amount(0.0), locked: false},
+            ];
+
+            let result = run(&records);
+
+            assert_eq!(result, expected);
+        }
+
+        //three clients w/ disputes: pending, resolved, and charged back
+        {
+            let records = vec![
+                InputRecord{r#type: "deposit".to_string(), client: 1, tx: 616, amount: Some(10.0)},
+                InputRecord{r#type: "deposit".to_string(), client: 2, tx: 525, amount: Some(10.0)},
+                InputRecord{r#type: "deposit".to_string(), client: 3, tx: 434, amount: Some(10.0)},
+
+                InputRecord{r#type: "dispute".to_string(), client: 1, tx: 616, amount: None},
+                InputRecord{r#type: "dispute".to_string(), client: 2, tx: 525, amount: None},
+                InputRecord{r#type: "dispute".to_string(), client: 3, tx: 434, amount: None},
+
+                InputRecord{r#type: "resolve".to_string(), client: 2, tx: 525, amount: None},
+                InputRecord{r#type: "chargeback".to_string(), client: 3, tx: 434, amount: None},
+
+                InputRecord{r#type: "withdrawal".to_string(), client: 3, tx: 343, amount: Some(5.0)},
+                InputRecord{r#type: "withdrawal".to_string(), client: 2, tx: 252, amount: Some(5.0)},
+                InputRecord{r#type: "withdrawal".to_string(), client: 1, tx: 161, amount: Some(5.0)},
+            ];
+
+            let expected = vec![
+                AccountState{client_id: ClientId(1), available: Amount(0.0), held: Amount(10.0), locked: false},
+                AccountState{client_id: ClientId(2), available: Amount(5.0), held: Amount(0.0), locked: false},
+                AccountState{client_id: ClientId(3), available: Amount(0.0), held: Amount(0.0), locked: true},
+            ];
+
+            let result = run(&records);
+
+            assert_eq!(result, expected);
+        }
+
+        //disputes + resolutions: wrong clients/transaction IDs
+        {
+            let records = vec![
+                InputRecord{r#type: "deposit".to_string(), client: 1, tx: 616, amount: Some(10.0)},
+                InputRecord{r#type: "deposit".to_string(), client: 2, tx: 525, amount: Some(10.0)},
+                InputRecord{r#type: "deposit".to_string(), client: 3, tx: 434, amount: Some(10.0)},
+
+                //wrong client
+                InputRecord{r#type: "dispute".to_string(), client: 2, tx: 616, amount: None},
+
+                //non-existent client
+            //todo: re-add this
+            //    InputRecord{r#type: "dispute".to_string(), client: 5, tx: 525, amount: None},
+
+                //valid
+                InputRecord{r#type: "dispute".to_string(), client: 3, tx: 434, amount: None},
+
+                //wrong transaction ID
+                InputRecord{r#type: "resolve".to_string(), client: 2, tx: 434, amount: None},
+
+                //wrong client
+                InputRecord{r#type: "chargeback".to_string(), client: 2, tx: 434, amount: None},
+            ];
+
+            let expected = vec![
+                AccountState{client_id: ClientId(1), available: Amount(10.0), held: Amount(0.0), locked: false},
+                AccountState{client_id: ClientId(2), available: Amount(10.0), held: Amount(0.0), locked: false},
+                AccountState{client_id: ClientId(3), available: Amount(0.0), held: Amount(10.0), locked: false},
             ];
 
             let result = run(&records);
